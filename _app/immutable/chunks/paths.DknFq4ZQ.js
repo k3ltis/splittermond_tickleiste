2e205440354099027939c8f659413eb3a23c475a
Thomas Kliche <t10e@mailbox.org>
2024-10-26 15:01:48 +0200

    Updates

---
+++ _app/immutable/chunks/paths.DknFq4ZQ.js
@@ -0,0 +1,1 @@
+var s;const e=((s=globalThis.__sveltekit_z1uttg)==null?void 0:s.base)??"/splittermond-tickleiste";var t;const a=((t=globalThis.__sveltekit_z1uttg)==null?void 0:t.assets)??e;export{a,e as b};
---
+++ _app/immutable/entry/start.BCeXlEUR.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.BhHRK1Eh.js";export{t as start};
